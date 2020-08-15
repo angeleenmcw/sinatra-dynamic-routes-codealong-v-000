71eba8bd980940c68656df26fc2c39316bd1ba9c
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-08-15 04:52:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,16 +11,8 @@ class App < Sinatra::Base
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
-    "Hello #{params[:name]}"
   end
 
-  get '/medicines/:id' do
-    @medicine = all_the_medicines.select do |medicine|
-      medicine.id == params[:id]
-    end.first 
-    erb :'/medicines/show.html'
-  end
-  
   get '/goodbye/:name' do 
     @user_name = params[:name]
     "Goodbye #{@user_name}!"
